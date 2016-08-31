a42736fb59c17d06d9dbca5059246df0230c0493
Alexandru Bagu <contact@alexandrubagu.info>
2016-08-31 22:17:52 +0300

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -17,17 +17,14 @@ defmodule SimpleHttp.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     []
+  end
+
+  defp description do
+    """
+    HTTP client for Elixir without dependencies.
+    """
   end
 
   defp package do
