b1d5e9c276cf31c4af19b8a8c5d5aa01272407b8
Alexandru Bagu <contact@alexandrubagu.info>
2016-08-31 22:19:51 +0300

    update mix.exs

--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule SimpleHttp.Mixfile do
     [app: :simplehttp,
      version: "0.1.0",
      elixir: "~> 1.3",
+     description: description(),
+     package: package(),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
@@ -31,7 +33,7 @@ defmodule SimpleHttp.Mixfile do
     [# These are the default files included in the package
       name: :simplehttp,
       description: "HTTP client for Elixir without dependencies",
-      files: ["lib", "config", "priv", "mix.exs", "README*", "readme*", "LICENSE*", "license*"],
+      files: ["lib", "config", "mix.exs", "README*"],
       maintainers: ["Bagu Alexandru Bogdan"],
       licenses: ["Apache 2.0"],
       links: %{"GitHub" => "https://github.com/alexandrubagu/simplehttp", "Docs" => "https://github.com/alexandrubagu/simplehttp", "Website" => "http://www.alexandrubagu.info" }]
