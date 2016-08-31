7664b413bc582c705b8dfcb5cf0f37823e867e22
Alexandru Bagu <contact@alexandrubagu.info>
2016-08-31 21:08:11 +0300

    added mix

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SimpleHttp.Mixfile do
 
   def project do
     [app: :simplehttp,
-     version: "1.0",
+     version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
