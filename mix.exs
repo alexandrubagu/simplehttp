613babf0770a578949cecabf66b4dcad77da44ca
Alexandru Bagu <contact@alexandrubagu.info>
2016-09-07 21:20:30 +0300

    add decmacro __using__ and fix http_options

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SimpleHttp.Mixfile do
 
   def project do
     [app: :simplehttp,
-     version: "0.1.0",
+     version: "0.2.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
