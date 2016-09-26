6db2bf496f488bdbf3b8a2ab28e4131ae0ee999a
Alexandru Bagu <contact@alexandrubagu.info>
2016-09-26 11:07:20 +0300

    change package version for publishing to hex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule SimpleHttp.Mixfile do
 
   def project do
     [app: :simplehttp,
-     version: "0.2.0",
+     version: "0.4.0",
      elixir: "~> 1.3",
      description: description(),
      package: package(),
