f6f725fc23288686797ee1579e801bff0760e0e1
Alexandru Bagu <contact@alexandrubagu.info>
2017-02-13 14:00:58 +0200

    fix warning

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule SimpleHttp.Mixfile do
   def project do
     [
       app: :simplehttp,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.3",
       description: description(),
       package: package(),
