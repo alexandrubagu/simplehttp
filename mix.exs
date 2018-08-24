64e5f82287c48d7bf3a263d9a5a6b4d8bb17bde5
alexandru.bagu <contact@alexandrubagu.info>
2018-08-24 17:09:57 +0300

    change ex_doc to remote warnings

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule SimpleHttp.Mixfile do
 
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:ex_doc, "~> 0.18.0", only: :dev},
       {:cowboy, "~> 1.0.4", only: :test},
       {:plug, ">= 1.2.0", only: :test},
       {:excoveralls, github: "parroty/excoveralls", only: :test}
